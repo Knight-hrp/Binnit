45dbbfb7e0389f1205a231c2e1dac9f0c1001a54
Knight-hrp <patidarrishabh20@gmail.com>
2025-03-27 20:10:40 +0530

    chatbar update

--- routes/community.js
+++ routes/community.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const { createCommunity, getCommunity,joinCommunity,exploreCommunity } = require('../controllers/community');
 const router = express.Router();
 const JOINCOMMUNITY = require('../models/joinCommunity');
+const path = require('path');
 const {} = require('../socketHandler')
 
 
@@ -51,8 +52,12 @@ router.post("/:community/postFile",upload.single("image"),(req,res)=>{
     }
     console.log("Image received:", req.file.filename);
     const filePath = `/communityImage/${req.file.filename}`;
+    const fileExtension = path.extname(req.file.originalname).toLowerCase();
     const room = req.params.community;
-    global.io.to(room).emit("newImage", filePath); 
+    global.io.to(room).emit("newMedia", {
+        filePath,
+        extension: fileExtension
+    }); 
     res.json({ message: "successfully!", filename: filePath });
 })
 
